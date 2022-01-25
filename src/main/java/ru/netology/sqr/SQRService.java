4cae4c362295e2a95e1e7028ffb050550063ba3c
Anna-64 <shurygina6410@gmail.com>
2022-01-25 23:48:57 +0400

    added test

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,20 +1,19 @@
 package ru.netology.sqr;
 
-import org.w3c.dom.ls.LSOutput;
-
 public class SQRService {
     public int squareAmount(int lowerBound, int upperBound) {
         int count = 0;
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 10; i <= 99; i++) {
             if (i * i >= lowerBound && i * i <= upperBound) {
                 System.out.println((" Корень числа:  " + i) + (" Число: " + i * i));
                 count = count + 1;
+            }
         }
-    }
-        System.out.println(" Количество квадратов "+ count);
+        System.out.println(" Количество квадратов " + count);
         return count;
+    }
 }
-}
+
 
 
 
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -8,9 +8,19 @@ class SQRServiceTest {
     @Test
     public void shouldSquareAmount() {
         SQRService service = new SQRService();
-        int expected = 6;
+        int expected = 1;
 
-        int actual = service.squareAmount(10, 99);
+        int actual = service.squareAmount(10, 100);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSquareNumberRange() {
+        SQRService service = new SQRService();
+        int expected = 3;
+
+        int actual = service.squareAmount(200, 300);
 
         assertEquals(expected, actual);
     }
